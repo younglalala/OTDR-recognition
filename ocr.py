fce6af15a75f40d686c02f67b731e4a0455e2d7e
young wang <38600556+younglalala@users.noreply.github.com>
2021-03-16 18:05:33 +0800

    OTDR内容提取规则

--- ocr.py
+++ ocr.py
@@ -12,6 +12,28 @@ DetectionModelPath = '/node4_gpu_nfs_raid10/wy/a_shandong/OTDR-recognition-data/
 ORDR_text_detection = OTDRTextDetection(model_path=DetectionModelPath,gpu_id='-1')
 
 
+def OCR_rules(all_bboxes_list,all_res_list,w,h):
+    """
+    根据识别结果提取相应信息
+    1、获取整张图片的2/h，2/3w的区域，即左下角区域；
+    2、在左下角区域进行正则匹配找到“结束”区域的位置，匹配“结束，结，束，吉”，并且字符中文长度小于4的作为匹配成功；
+    3、结束区域找到向右横向的所有框，并且确认框内全为数字内容；
+    4、找到所有数字内容了根据一下标注进行提取：
+    位置：0.0000
+    反射-00.0
+    衰减：0.000
+    累计损耗：0.00
+    插损：无
+
+
+    :param all_bboxes_list: 文字片段坐标点信息
+    :param all_res_list: 文字片段识别结果
+    :param w: 整张图片的宽
+    :param h: 整张图片的高
+    :return: 位置（location）、反射（reflection）、衰减（attenuation）、累计损耗（stack_loss）、插损的值（insertion_loss)）。
+
+    """
+
 
 
 
@@ -28,20 +50,19 @@ if __name__ =='__main__':
 
         h, w, _ = img.shape
 
-        all_out = []
-        all_out_crnn = []
-        print('*-'*10,file,'*-'*10)
+        all_res_out = []
+
         for ind, crop_im in enumerate(all_crop_img):
             image = Image.fromarray(crop_im)
             crnn_preds, crnn_pred_prob = predict.predict_prob(image, lan='chinese', useStr=None)
-            print(crnn_preds)
             bbox = all_crop_bbox[ind]
-            print(bbox)
-            xmin, ymin, xmax, ymax = map(int, [min(bbox[:, 0]), min(bbox[:, 1]), max(bbox[:, 0]), max(bbox[:, 1])])
+            all_res_out.append(crnn_preds)
 
 
-            cv2.rectangle(src_image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-            cv2.imwrite(os.path.join(save_path,file),src_image)
-            x1,y1,x2,y2,x3,y3,x4,y4 = bbox[0][0],bbox[0][1],bbox[1][0],bbox[1][1],bbox[2][0],bbox[2][1],bbox[3][0],bbox[3][1]
-            f.writelines('{},{},{},{},{},{},{},{},{}\n'.format(x1,y1,x2,y2,x3,y3,x4,y4,crnn_preds))
-        print('*-' * 10, file, '*-' * 10)
+
+            # xmin, ymin, xmax, ymax = map(int, [min(bbox[:, 0]), min(bbox[:, 1]), max(bbox[:, 0]), max(bbox[:, 1])])
+
+
+            # cv2.rectangle(src_image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
+            #
+            # x1,y1,x2,y2,x3,y3,x4,y4 = bbox[0][0],bbox[0][1],bbox[1][0],bbox[1][1],bbox[2][0],bbox[2][1],bbox[3][0],bbox[3][1]
